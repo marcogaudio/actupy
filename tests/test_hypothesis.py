0058afb5cf6e0ad5bb29b268083fd5ff02a06e29
marcogaudio <marcogaudio1997@outlook.it>
2025-01-12 11:52:47 +0100

    adding test for hypothesis class

--- tests/test_hypothesis.py
+++ tests/test_hypothesis.py
@@ -2,6 +2,22 @@ from hypothesis import Hypothesis
 import pandas as pd
 import pytest
 
+def test_mortality_is_dataframe():
+    mortality_table = pd.DataFrame({
+        "age": [20, 21, 22, 23, 24, 25],
+        "qx": [0.001, 0.0012, 0.0015, 0.0017, 0.002, 0.0022]
+    })
+    hypothesis = Hypothesis(mortality=mortality_table)
+    assert hypothesis.mortality.equals(mortality_table)
+
+def test_mortality_is_dataframe_wrong_attributes():
+    mortality_table = pd.DataFrame({
+        "current_AGE": [20, 21, 22, 23, 24, 25],
+        "prob": [0.001, 0.0012, 0.0015, 0.0017, 0.002, 0.0022]
+    })
+    with pytest.raises(ValueError):
+        Hypothesis(mortality=mortality_table)
+
 def test_mortality_is_integer():
     with pytest.raises(TypeError):
         Hypothesis(mortality=123)
@@ -14,26 +30,32 @@ def test_mortality_is_none():
     hypothesis = Hypothesis(mortality=None)
     assert hypothesis.mortality.empty
 
+# lapse
 
+def test_lapse_is_dataframe():
+    lapse_table = pd.DataFrame({
+        "duration": [1, 2, 3, 4, 5, 6],
+        "rx": [0.05, 0.04, 0.03, 0.02, 0.01, 0.005]
+    })
+    hypothesis = Hypothesis(lapse=lapse_table)
+    assert hypothesis.lapse.equals(lapse_table)
 
-test = Hypothesis(mortality=None, lapse=0.02, loe=0.005, ci=0.0005)
-print(test)
+def test_lapse_is_dataframe_wrong_attributes():
+    lapse_table = pd.DataFrame({
+        "time": [1, 2, 3, 4, 5, 6],
+        "rate": [0.05, 0.04, 0.03, 0.02, 0.01, 0.005]
+    })
+    with pytest.raises(ValueError):
+        Hypothesis(lapse=lapse_table)
 
+def test_lapse_is_integer():
+    with pytest.raises(TypeError):
+        Hypothesis(lapse=123)
 
-""""
-# Crea un DataFrame con i valori di mortalità
-mortality_table = pd.DataFrame({
-    "age": [20, 21, 22, 23, 24, 25],
-    "qx": [0.001, 0.0012, 0.0015, 0.0017, 0.002, 0.0022]
-})
+def test_lapse_is_double():
+    with pytest.raises(TypeError):
+        Hypothesis(lapse=123.456)
 
-# Inizializza l'oggetto Hypothesis con la tabella di mortalità
-hypothesis = Hypothesis(mortality=mortality_table, lapse=0.02, loe=0.005, ci=0.0005)
-
-# Stampa l'oggetto
-print(hypothesis)
-
-# Ottieni il valore di mortalità per un'età specifica
-qx_for_age_23 = hypothesis.get_mortality_for_age(23)
-print(f"Probabilità di morte all'età di 23 anni: {qx_for_age_23}")
-"""
+def test_lapse_is_none():
+    hypothesis = Hypothesis(lapse=None)
+    assert hypothesis.lapse.empty
